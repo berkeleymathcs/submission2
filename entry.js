729b3b9bda5557428099eb4166f60fb4dc1b470a
berkeleymathcs <rcanastra@berkeley.edu>
2014-04-19 15:31:57 -0700

    Create entry.js

---
+++ entry.js
@@ -0,0 +1,37 @@
+Bot.register('PenguinCoder', function(game_state, player_state, move) {
+  var me = game_state.me;
+  var board = game_state.board;
+  var moves = board.safe_directions(me);
+
+  if (me.last_move == 4 && _.contains(moves, 4) && me.y < 13) {
+    move(4);
+  } else if (me.y == 13 && me.x > 1 && _.contains(moves,2)) {
+    move(2);
+  } else if (me.x < 2 && _.contains(moves,4)) {
+    move(4);
+  } else if ((me.x + me.y) % 2 == 0 && me.y == 0 && _.contains(moves, 2) && !_.contains(moves, 4)) {
+    move(2);
+  } else if (_.contains(moves,1) && !_.contains(moves, 0)) {
+    move(1);
+  } else if (_.contains(moves, 4) && !_.contains(moves, 0)) {
+    move(4);
+  } else if (!_.contains(moves, 4) && _.contains(moves, 3) && !_.contains(moves, 1)) {
+    move(3);
+  } else if (_.contains(moves, 1) && !_.contains(moves, 0)) {
+    move(1);
+  } else if (me.y == 0 && _.contains(moves, 2)) {
+    move(2);
+  } else if (me.y == 0 && _.contains(moves, 3)) {
+    move(3);
+  } else if (_.contains(moves, 0)) {
+    move(0);
+  } else if (_.contains(moves, 1)) {
+    move(1);
+  } else {
+    for (var i = 0; i < 6; i ++) {
+      if (_.contains(moves, i)) {
+        move(i);
+      }
+    }
+  }
+})
